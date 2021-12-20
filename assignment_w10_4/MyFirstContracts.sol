db38e4e40db4856cac9056cb68481efbe5f45004
DuaaAlgethami <63024859+DuaaAlgethami@users.noreply.github.com>
2021-12-20 11:41:51 +0300

    Update MyFirstContracts.sol

--- assignment_w10_4/MyFirstContracts.sol
+++ assignment_w10_4/MyFirstContracts.sol
@@ -15,22 +15,19 @@ contract Bank{
     mapping(address=>uint) public account_balances;
 
     function transfer(uint amount, address acctToTransferTo) external {
-
+        require(account_balances[msg.sender]>amount, "Insufficent Funds!");
         account_balances[msg.sender] -= amount;
         account_balances[acctToTransferTo] += amount;
     }
 
 
     function withdraw(uint amount) external {
-
+        require(account_balances[msg.sender]>amount, "Insufficent Funds!");
         account_balances[msg.sender] -= amount;
         payable(msg.sender).transfer(amount);
+
     }
 
     receive () external payable{
-        account_balances[msg.sender] += msg.value;
+        account_balances[msg.sender] += msg.value;//delet
     }
-
-    
-
-}
